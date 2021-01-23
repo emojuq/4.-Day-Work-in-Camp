bdc643083ebf6bcc325c61998ebf376a97b17225
Emre Aydın <gamertremre@gmail.com>
2021-01-23 19:44:55 +0300

    Add project files.

---
+++ Ödev - 4. Gün/MyDictionary.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ödev___4._Gün
+{
+    class MyDictionary<TKey,TValue >
+    {
+        TKey[] _key;
+        TValue[] _value;
+        public MyDictionary()
+        {
+            _key = new TKey[0];
+
+            _value = new TValue[0];
+        }
+
+        public void Add( TKey key , TValue value)
+        {
+            TKey[] keyler = _key;
+            TValue[] valueler = _value;
+
+            _key = new TKey[_key.Length + 1];
+            _value = new TValue[_value.Length + 1];
+
+            for (int i = 0; i <keyler.Length; i++)
+            {
+                _value[i] = valueler[i];
+                _key[i] = keyler[i];
+            }
+
+            _key[_key.Length - 1] = key;
+            _value[_value.Length - 1] = value;
+
+        }
+
+        public int Count
+        {
+            get { return _key.Length; }
+        }
+
+        public TKey[] key
+        {
+            get { return _key; }
+        }
+
+        public TValue[] value
+        {
+            get { return _value; }
+        }
+      
+            
+
+    }
+}
---
+++ Ödev - 4. Gün/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ödev___4._Gün
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> ArabaMarkaları = new MyDictionary<int, string>();
+            ArabaMarkaları.Add(1,"Mercedes");
+            ArabaMarkaları.Add(2, "BMW");
+            ArabaMarkaları.Add(3, "Volvo");
+            ArabaMarkaları.Add(4, "Honda");
+            ArabaMarkaları.Add(5, "Hyundai");
+            ArabaMarkaları.Add(6, "Renault");
+
+            for (int i = 0; i <ArabaMarkaları.key.Length; i++)
+            {
+                Console.WriteLine("Araba Numarası" + " " + ArabaMarkaları.key[i]);
+                Console.WriteLine("Araba Markası" + " " +  ArabaMarkaları.value[i]);
+                Console.WriteLine("------------------------------------------");
+
+            }
+
+        }
+    }
+}
